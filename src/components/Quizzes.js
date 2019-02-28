de2d838331510d7457beb9c49fb5b2df9ee45eb2
Tao Ong <taoong@berkeley.edu>
2019-02-28 10:01:41 -0800

    Can read quiz data from firestore

--- src/components/Quizzes.js
+++ src/components/Quizzes.js
@@ -1,7 +1,60 @@
 import React, { Component } from "react";
+import firebase from "../Firebase";
 class Quizzes extends Component {
+  state = { quizzes: [] };
+  quizzesRef = firebase.firestore().collection("quizzes");
+
+  componentDidMount = () => {
+    let currentComponent = this;
+    this.quizzesRef.get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        currentComponent.setState(prevState => ({
+          quizzes: [...prevState.quizzes, doc]
+        }));
+      });
+    });
+  };
+
+  addQuiz() {
+    console.log("Add quiz");
+  }
+
+  renderQuizRows() {
+    if (this.state.quizzes.length > 0) {
+      const quizzes = this.state.quizzes.map((quiz, index) => (
+        <tr key={index}>
+          <td>{quiz.id}</td>
+          <td>{quiz.data().name}</td>
+          <td>{quiz.data().audience}</td>
+          <td>{quiz.data().type}</td>
+        </tr>
+      ));
+      return quizzes;
+    }
+  }
+
   render() {
-    return <h1>Quizzes</h1>;
+    const quizzes = this.renderQuizRows();
+
+    return (
+      <div className="quizzes">
+        <div className="header-div">
+          <h1>Quizzes</h1>
+          <button onClick={this.addQuiz}>&#10010;</button>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <th>ID</th>
+              <th>Quiz Name</th>
+              <th>Audience Type</th>
+              <th>Question-Answer Type</th>
+            </tr>
+            {quizzes}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 export default Quizzes;
