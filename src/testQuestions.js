288a5c72a9c2626f5e1a8c8c24280590a510b453
Tao Ong <taoong@berkeley.edu>
2019-04-09 23:58:05 -0700

    Adding stimuli

--- src/testQuestions.js
+++ src/testQuestions.js
@@ -1,36 +1,57 @@
 var quizQuestions = [
   {
-    question: "What emotion does this image represent?",
-    type: "face-text",
-    imageUrl: "images/face1.jpg",
-    answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
-    correctAnswer: "Happiness"
+    type: "voice-face",
+    question: "./Stimuli/Bursts/Bursts/VENEC/0278.mp3",
+    answers: [
+      "./Stimuli/EI Quiz/Disgust.jpg",
+      "./Stimuli/EI Quiz/Embarassment.jpg",
+      "./Stimuli/EI Quiz/Fear.jpg",
+      "./Stimuli/EI Quiz/Interest.jpg"
+    ],
+    correctAnswer: "./Stimuli/EI Quiz/Disgust.jpg"
   },
   {
-    question: "What emotion does this image represent?",
-    type: "face-text",
-    imageUrl: "images/face2.jpg",
-    answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
-    correctAnswer: "Satisfaction"
+    type: "face-voice",
+    question: "./Stimuli/EI Quiz/desire.jpg",
+    answers: [
+      "./Stimuli/Bursts/Bursts/Leah/073.mp3",
+      "./Stimuli/Bursts/Bursts/Leah/074.mp3.mp3",
+      "./Stimuli/Bursts/Bursts/Leah/075.mp3",
+      "./Stimuli/Bursts/Bursts/Leah/076.mp3"
+    ],
+    correctAnswer: "./Stimuli/Bursts/Bursts/Leah/073.mp3"
   },
   {
-    question: "What emotion does this image represent?",
-    type: "face-text",
-    imageUrl: "images/face3.jpg",
-    answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
-    correctAnswer: "Happiness"
+    type: "illustration-face",
+    question: "./Stimuli/Illustrations/pain.png",
+    answers: [
+      "./Stimuli/EI Quiz/Pain.jpg",
+      "./Stimuli/EI Quiz/Interest.jpg",
+      "./Stimuli/EI Quiz/Pride.jpg",
+      "./Stimuli/EI Quiz/Surprise.jpg"
+    ],
+    correctAnswer: "./Stimuli/EI Quiz/Pain.jpg"
   },
   {
-    question: "What emotion does this image represent?",
-    type: "face-text",
-    imageUrl: "images/face4.jpg",
-    answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
-    correctAnswer: "Surprise"
+    type: "illustration-voice",
+    question: "images/face4.jpg",
+    answers: [
+      "./Stimuli/Bursts/Bursts/Allen/010.mp3",
+      "./Stimuli/Bursts/Bursts/Allen/011.mp3",
+      "./Stimuli/Bursts/Bursts/Allen/012.mp3",
+      "./Stimuli/Bursts/Bursts/Allen/013.mp3"
+    ],
+    correctAnswer: "./Stimuli/Bursts/Bursts/Allen/010.mp3"
   },
   {
-    question: "What emotion does this image represent?",
-    type: "face-text",
-    imageUrl: "images/face5.jpg",
+    type: "story-face",
+    question: "images/face5.jpg",
+    answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
+    correctAnswer: "Fear"
+  },
+  {
+    type: "story-voice",
+    question: "images/face5.jpg",
     answers: ["Happiness", "Satisfaction", "Surprise", "Fear"],
     correctAnswer: "Fear"
   }
