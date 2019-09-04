4b6b94778753e475f78972d436c6e608e9799150
damnabit <adavid@artsmia.org>
2019-09-04 13:19:44 -0500

    Generalizing Firebase.js again

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const config = {
-    apiKey: "AIzaSyCHB1ytyh_ks-Ts3Vfao3JHKCcuDDekxH4",
-    authDomain: "empathy3-df99f.firebaseapp.com",
-    databaseURL: "https://empathy3-df99f.firebaseio.com",
-    projectId: "empathy3-df99f",
-    storageBucket: "empathy3-df99f.appspot.com",
-    messagingSenderId: "63510853296",
-    appId: "1:63510853296:web:4a4ff14436a08454"
+   /* Your Firebase SDK Config goes here */
 };
 firebase.initializeApp(config);
 
