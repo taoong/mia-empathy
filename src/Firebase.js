f14f8652bcb674b68c4b67c7ee5c5f7d0ccf89c1
damnabit <adavid@artsmia.org>
2019-09-04 12:59:23 -0500

    Added generic marker for Firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const config = {
-  apiKey: "AIzaSyCKLScx-8SzmQCr6us-a7x598O1bmqu15M",
-  authDomain: "mia-empathy.firebaseapp.com",
-  databaseURL: "https://mia-empathy.firebaseio.com",
-  projectId: "mia-empathy",
-  storageBucket: "mia-empathy.appspot.com",
-  messagingSenderId: "679221564571"
+   /* Insert your Firebase Config here */
 };
 firebase.initializeApp(config);
 
