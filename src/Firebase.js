bb78ee2feef9aa6c21099f5444da15ac1a60bc8f
Tao Ong <taoong@berkeley.edu>
2019-11-14 17:58:45 -0800

    Including full MIA test stimuli and removing unneeded stimuli

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,7 +4,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const config = {
-   /* Your Firebase SDK Config goes here */
+  apiKey: "AIzaSyCKLScx-8SzmQCr6us-a7x598O1bmqu15M",
+  authDomain: "mia-empathy.firebaseapp.com",
+  databaseURL: "https://mia-empathy.firebaseio.com",
+  projectId: "mia-empathy",
+  storageBucket: "mia-empathy.appspot.com",
+  messagingSenderId: "679221564571",
+  appId: "1:679221564571:web:a9273dd883ecbf2b4e223e"
 };
 firebase.initializeApp(config);
 
